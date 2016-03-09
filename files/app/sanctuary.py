2e1d23d0376d2044d0bfe9329f7877dfcb5d6965
Erin Corson <erin@newmediadenver.com>
2016-03-08 17:04:23 -0700

    some initial work

--- files/app/sanctuary.py
+++ files/app/sanctuary.py
@@ -18,12 +18,21 @@ def sanctuary():
 
 @sanctuary.command()
 def generate_ami():
+    """Creates template AMI to be used in AWS auto-scaling groups."""
     run_playbook('ami')
 
 
 @sanctuary.command()
 def build():
+    """Uses the vault AMI to build out an HA Vault service."""
     run_playbook('create')
+
+
+@sanctuary.command()
+def create():
+    """Build the AMI and create the Vault service."""
+    generate_ami()
+    build()
 
 
 @sanctuary.command()
@@ -32,9 +41,23 @@ def delete():
 
 def run_playbook(playbook):
     run_command = "ansible-playbook /app/{playbook}.yml -i 'localhost,' -c local".format(playbook=playbook)
-    subprocess.call(shlex.split(run_command), env=os.environ.copy())
+    #subprocess.call(shlex.split(run_command), env=os.environ.copy())
+    sub_process = subprocess.Popen(
+        run_command,
+        close_fds=True,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        env=os.environ.copy()
+    )
 
+    while sub_process.poll() is None:
+        out = sub_process.stdout.read(1)
+        sys.stdout.write(out)
+        sys.stdout.flush()
+
+    if sub_process.returncode:
+        sys.exit(1)
 
 if __name__ == '__main__':
     sanctuary()
-
